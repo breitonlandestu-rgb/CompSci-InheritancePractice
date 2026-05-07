b6d72b13c60c59e1b0a8a96ea60193ed5527996e
breitonlandestu-rgb <breiton.lande.stu@cedarpark.org>
2026-05-07 09:25:11 -0700

    hi

---
+++ Untitled-1.py
@@ -0,0 +1,33 @@
+
+
+
+
+classVehicle:
+
+def__init__(self, make, model):
+
+self.make = make
+
+self.model = model
+
+def start(self):
+
+return f"{self.make} {self.model} starting..."
+
+classCar(Vehicle):
+
+def__init__(self, make, model, doors):
+
+super().__init__(make, model)
+
+self.doors = doors
+
+def start(self):
+
+base = super().start()
+
+return f"{base} vroom!"
+
+# Create and use the car
+my_car = Car("Toyota", "Corolla", 4)
+ print(my_car.start())
